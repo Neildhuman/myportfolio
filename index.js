e5cce58c82ea223e679b94d48bad301511ab7553
Neildhuman <neil.paypertask@gmail.com>
2024-04-04 05:22:41 +0800

    Updated index.css/.html/.js files

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
-document.addEventListener("DOMContentLoaded", function() {
-        const listToggle = document.querySelector('.list-toggle');
-        const listItems = document.querySelectorAll('.list li');
+// document.addEventListener("DOMContentLoaded", function() {
+//         const listToggle = document.querySelector('.list-toggle');
+//         const listItems = document.querySelectorAll('.list li');
 
-        listToggle.addEventListener('click', function() {
-            listItems.forEach(function(item) {
-                item.style.display = (item.style.display === 'none' || item.style.display === '') ? 'block' : 'none';
-            });
-        });
+//         listToggle.addEventListener('click', function() {
+//             listItems.forEach(function(item) {
+//                 item.style.display = (item.style.display === 'none' || item.style.display === '') ? 'block' : 'none';
+//             });
+//         });
+//     });
+
+document.addEventListener('DOMContentLoaded', function () {
+    const listToggle = document.querySelector('.list-toggle');
+    const list = document.querySelector('.list ol');
+
+    listToggle.addEventListener('click', function () {
+        list.classList.toggle('active');
     });
+});
